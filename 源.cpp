9e33ffc9c45c6d70778f214eef83fc4c70aaaf0b
only123hhl <37827519+only123hhl@users.noreply.github.com>
2019-03-12 15:36:08 +0800

    Algorithm-2(Floyd)

--- 源.cpp
+++ 源.cpp
@@ -1,24 +1,43 @@
 #include<iostream>
 using namespace std;
-int Fib( int A,int B, int n)
+const int inf = 99999;
+int shortlength(int n, int x, int y, int** array)
 {
-   int* f = new int[n + 1];
-	f[1] = 1, f[2] = 1;
-	for (int i = 3; i <= n; i++)
-		f[i] = (A*f[i - 1] + B*f[i - 2]) % 7;
-   int r = f[n];
-	delete f;
-	return r;
+	for(int m=1;m<=n;m++)
+		for(int i=1;i<=n;i++)
+			for(int j=1;j<=n;j++)
+	           if (array[i][m] + array[m][j] < array[i][j])
+		             array[i][j] = array[i][m] + array[m][j];
+	int temp = array[x][y];
+	delete array;
+	if (temp == inf)
+		return 0;
+	return temp;
 }
 int main()
 {
-	int A, B;
-	long long int n;
-	cin >> A >> B >> n;
-	int temp = n % 49;
-		if (A == 0 && B == 0 && n == 0)
-			return 0;
-		cout << Fib(A, B, temp);
-	
+	int n, m;
+	cin >> n >> m;
+	int** array = new int*[n+1];
+	for (int i = 1; i <= n; i++)
+		array[i] = new int[n];
+	//初始化数组
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			if (i == j)
+				array[i][j] = 0;
+			else
+			    array[i][j] = inf;
+	//开始输入
+	for (int i = 0; i < m; i++)
+	{
+		int city1, city2, length;
+		cin >> city1 >> city2 >> length;
+		array[city1][city2] = length;
+		array[city2][city1] = length;
+	}
+	int x, y;
+	cin >> x >> y;
+	cout << shortlength(n, x, y, array);
 	system("pause");
 }
